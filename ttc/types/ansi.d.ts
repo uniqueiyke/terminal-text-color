e277022fd677d9e135606317ca22fe986ae2393f
Ikechukwu Ajike <iykeisunique@gmail.com>
2023-10-21 17:33:39 +0100

    some changes

---
+++ ttc/types/ansi.d.ts
@@ -0,0 +1,10 @@
+declare const ttcColorApi: {
+    fgColor: (n: number) => string;
+    bgColor: (n: number) => string;
+    fgColorRGB: (r: number, g: number, b: number) => string;
+    bgColorRGB: (r: number, g: number, b: number) => string;
+    fgColorHex: (hex: string) => string;
+    bgColorHex: (hex: string) => string;
+};
+export default ttcColorApi;
+//# sourceMappingURL=ansi.d.ts.map
---
+++ ttc/types/basics.d.ts
@@ -0,0 +1,13 @@
+declare const ttcModifiers: {
+    reset: () => string;
+    dim: (text?: string) => string;
+    italic: (text?: string) => string;
+    bold: (text?: string) => string;
+    underline: (text?: string) => string;
+    reverse: (text?: string) => string;
+    hidden: (text?: string) => string;
+    strikethrough: (text?: string) => string;
+    blink: (text?: string) => string;
+};
+export default ttcModifiers;
+//# sourceMappingURL=basics.d.ts.map
